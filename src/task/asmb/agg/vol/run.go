0d46c12ca5860f124b8a0113d11290545cc48571
cheolgyu <rb7480@knou.ac.kr>
2022-07-06 18:54:52 +0900

    datainfo 항목 재구성으로 가격콜렉션 리셋필요.

--- src/task/asmb/agg/vol/run.go
+++ src/task/asmb/agg/vol/run.go
@@ -5,10 +5,7 @@ import (
 
 	"github.com/cheolgyu/stockbot/src/common"
 	"github.com/cheolgyu/stockbot/src/common/doc"
-	"github.com/cheolgyu/stockbot/src/common/model"
-	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 var client *mongo.Client
@@ -16,6 +13,17 @@ var collection_price *mongo.Collection
 var collection_agg_vol *mongo.Collection
 var collection_agg_vol_sum *mongo.Collection
 
+/*
+절차
+	agg_vol_sum 계산범위:  코드별 마지막 가격데이터의 년도
+	1. 코드별 마지막가격 데이터의 년도에 해당하는 가격데이터의 조회한다.
+	2. 코드별 가격데이터에서 주별 거래량의합, 월별 거래량의 합, 분기별 거래량의합을 구한다.
+	3. 코드별 해당연도의 agg_vol_sum을 upsert 한다.
+	4. 코드별 전체연도의 agg_vol_sum을 조회한다.
+	5. 코드별 코드의 전체연도의 agg_vol_sum데이터로 기간별 퍼센트 값목록을 구한다.
+	6. 코드별 코드의 전체연도의 기간별 퍼센트 값목록을 저장한다.
+
+*/
 func Run() {
 	var ctx context.Context
 	client, ctx = common.Connect()
@@ -25,7 +33,9 @@ func Run() {
 	collection_agg_vol = client.Database(doc.DB_DATA).Collection(doc.DB_DATA_COLL_AGG_VOL)
 	collection_agg_vol_sum = client.Database(doc.DB_DATA).Collection(doc.DB_DATA_COLL_AGG_VOL_SUM)
 
-	// companys := doc.GetCompany()
+	panic(" datainfo 항목 재구성으로 가격콜렉션 리셋필요.    ")
+
+	//companys := doc.GetCompany()
 	// for _, v := range companys[:1] {
 	// 	prices := select_new_price_data(v.Code.Code, v.AggVolAt)
 
@@ -38,43 +48,44 @@ func Run() {
 	// }
 }
 
-func insert_new_price_data(price []model.PriceMarket) {
+// func insert_new_price_data(price []model.PriceMarket) {
 
-	//models := []mongo.WriteModel{}
+// 	models := []mongo.WriteModel{}
 
-	// for _, v := range price {
-	// 	i := model.AggVolSum{Code: v.Code,Year: v.Dt_y,PeriodType: ,VOL}
-	// 	filter := bson.M{"code": i.Code, "price_type": i.PriceType, "p1.x": i.P1.X}
-	// 	models = append(models, mongo.NewReplaceOneModel().SetFilter(filter).SetUpsert(true).SetReplacement(i))
-	// }
+// 	for _, v := range price {
+// 		i := model.AggVolSum{Code: v.Code,Year: v.Dt_y,PeriodType: ,VOL}
+// 		filter := bson.M{"code": i.Code, "price_type": i.PriceType, "p1.x": i.P1.X}
+// 		models = append(models, mongo.NewReplaceOneModel().SetFilter(filter).SetUpsert(true).SetReplacement(i))
+// 	}
 
-	// opts := options.BulkWrite().SetOrdered(false)
-	// res, err := collection_agg_vol_sum.BulkWrite(context.TODO(), models, opts)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// fmt.Printf(
-	// 	"inserted %v documents and upserted %v documents and ModifiedCount %v and deleted %v documents\n",
-	// 	res.InsertedCount,
-	// 	res.UpsertedCount,
-	// 	res.ModifiedCount,
-	// 	res.DeletedCount)
+// 	opts := options.BulkWrite().SetOrdered(false)
+// 	res, err := collection_agg_vol_sum.BulkWrite(context.TODO(), models, opts)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	fmt.Printf(
+// 		"inserted %v documents and upserted %v documents and ModifiedCount %v and deleted %v documents\n",
+// 		res.InsertedCount,
+// 		res.UpsertedCount,
+// 		res.ModifiedCount,
+// 		res.DeletedCount)
 
-}
+// }
 
-func select_new_price_data(code string, last int) []model.PriceMarket {
-	filter := bson.M{"code": code, "dt": bson.M{"$lt": last}}
-	opts := options.Find().SetSort(bson.M{"dt": -1})
+// 2. 코드별 마지막가격 데이터의 년도에 해당하는 가격데이터의 조회한다.
+// func select_price_list (code string) []model.PriceMarket {
+// 	filter := bson.M{"code": code, "dt": bson.M{"$lt": last}}
+// 	opts := options.Find().SetSort(bson.M{"dt": -1})
 
-	var results []model.PriceMarket
-	cursor, err := collection_price.Find(context.TODO(), filter, opts)
-	if err != nil {
-		panic(err)
-	}
+// 	var results []model.PriceMarket
+// 	cursor, err := collection_price.Find(context.TODO(), filter, opts)
+// 	if err != nil {
+// 		panic(err)
+// 	}
 
-	if err = cursor.All(context.TODO(), &results); err != nil {
-		panic(err)
-	}
-	return results
+// 	if err = cursor.All(context.TODO(), &results); err != nil {
+// 		panic(err)
+// 	}
+// 	return results
 
-}
+// }
